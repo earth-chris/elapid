00a1d88320e0c4b0d589dcf37eb89624520a9d82
earth-chris <cba@salo.ai>
2021-03-24 18:40:21 -0700

    added sample data loading

--- elapid/__init__.py
+++ elapid/__init__.py
@@ -1,3 +1,4 @@
 """Controlling user exposure"""
 
 from elapid.models import Maxent
+from elapid.utils import load_sample_data
--- elapid/utils.py
+++ elapid/utils.py
@@ -1,7 +1,9 @@
 """Backend helper functions that don't need to be exposed to users"""
 import multiprocessing as mp
+import os
 
 import numpy as np
+import pandas as pd
 
 _ncpus = mp.cpu_count()
 
@@ -32,3 +34,24 @@ def repeat_array(x, length=1, axis=0):
     :returns: an n+1 dimensional numpy array
     """
     return np.expand_dims(x, axis=axis).repeat(length, axis=axis)
+
+
+def load_sample_data(name="bradypus"):
+    """
+    Loads example species presence/background and covariate data.
+
+    :param name: the sample dataset to load. options currently include ["bradypus"], from the R 'maxnet' package
+    :returns: (x, y), a tuple of dataframes containing covariate and response data, respectively
+    """
+    assert name.lower() in ["bradypus"], "Invalid sample data requested"
+
+    package_path = os.path.realpath(__file__)
+    package_dir = os.path.dirname(package_path)
+
+    if name.lower() == "bradypus":
+
+        file_path = os.path.join(package_dir, "data", "bradypus.csv.gz")
+        df = pd.read_csv(file_path, compression="gzip").astype("int16")
+        y = df["presence"].astype("category")
+        x = df[df.columns[1:]].astype({"ecoreg": "category"})
+        return x, y
