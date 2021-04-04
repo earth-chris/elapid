236b592c788db8c862525f38fdf8e72925255a72
earth-chris <cba@salo.ai>
2021-04-04 10:44:18 -0700

    adding geo module

--- elapid/__init__.py
+++ elapid/__init__.py
@@ -1,5 +1,6 @@
 """Controlling user exposure"""
 
+from elapid import geo
 from elapid.features import MaxentFeatureTransformer
 from elapid.models import MaxentModel
 from elapid.utils import load_object, load_sample_data, save_object
