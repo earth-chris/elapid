3a4c37e93319b1665d0c84b88947b8702309a91d
earth-chris <cba@salo.ai>
2021-04-05 21:56:40 -0700

    adding specific geo functions

--- elapid/__init__.py
+++ elapid/__init__.py
@@ -1,6 +1,15 @@
 """Controlling user exposure"""
 
-from elapid import geo
 from elapid.features import MaxentFeatureTransformer
+from elapid.geo import (
+    apply_model_to_rasters,
+    pseudoabsence_from_bias_file,
+    pseudoabsence_from_geoseries,
+    pseudoabsence_from_raster,
+    pseudoabsence_from_vector,
+    raster_values_from_geoseries,
+    raster_values_from_vector,
+    xy_to_geoseries,
+)
 from elapid.models import MaxentModel
 from elapid.utils import load_object, load_sample_data, save_object
