72cfc4e3b4094d736409836e48cdf61cfa93516c
earth-chris <cba@salo.ai>
2022-01-17 12:53:16 -0800

    init model tests

---
+++ tests/test_models.py
@@ -0,0 +1,48 @@
+from sklearn import metrics
+
+from elapid import models
+from elapid.utils import load_sample_data
+
+x, y = load_sample_data()
+
+
+def test_MaxentModel_flow():
+    model = models.MaxentModel()
+    model.fit(x, y)
+    ypred = model.predict(x)
+    assert len(ypred) == len(y)
+    assert ypred.max() <= 1.0
+    assert ypred.min() >= 0.0
+
+
+def test_MaxentModel_performance():
+    model = models.MaxentModel(use_lambdas="last", scorer="roc_auc")
+    model.fit(x, y)
+    ypred = model.predict(x)
+    auc_score = metrics.roc_auc_score(y, ypred)
+
+    # check that model is not nonsense
+    assert 0.5 <= auc_score <= 1.0
+
+    # check that full model close to cv performance
+    assert abs(auc_score - model.estimator.cv_mean_score_[-1]) < 0.1
+
+
+def test_MaxentModel_feature_types():
+    all = models.MaxentModel(feature_types="lqpht", beta_multiplier=1.5)
+    linear = models.MaxentModel(feature_types=["linear"])
+    qp = models.MaxentModel(feature_types=["quadratic", "product"], convergence_tolerance=2e-7, beta_lqp=1.5)
+    ht = models.MaxentModel(
+        feature_types=["hinge", "threshold"],
+        beta_threshold=1.5,
+        beta_hinge=0.75,
+        n_threshold_features=10,
+        n_hinge_features=10,
+    )
+    auto = models.MaxentModel(feature_types=["auto"])
+
+    for model in [all, linear, qp, ht, auto]:
+        model.fit(x, y)
+        ypred = model.predict(x)
+        auc_score = metrics.roc_auc_score(y, ypred)
+        assert 0.5 <= auc_score <= 1.0
