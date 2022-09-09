19150c33d8fe12e7b6d94417ce499626ec21d13f
earth-chris <cba@salo.ai>
2022-09-08 21:58:23 -0700

    restoring requirements.txt

--- setup.py
+++ setup.py
@@ -6,20 +6,7 @@ from setuptools import setup
 this_dir, this_path = os.path.split(os.path.abspath(__file__))
 version = open(os.path.join(this_dir, "elapid", "__version__.py")).read().strip('"\n')
 long_description = open(os.path.join(this_dir, "README.md"), "r", encoding="utf-8").read()
-
-requirements = [
-    "descartes>=1.1.0",
-    "geopandas>=0.7.0",
-    "numpy>=1.18",
-    "pandas>=1.0.3",
-    "rasterio>=1.2.1",
-    "rtree>=0.9",
-    "scikit-learn>=0.22.2",
-    "shapely>=1.7.0",
-    "tqdm>=4.60.0",
-]
-if platform.system() != "Windows":
-    requirements.append("glmnet;python_version<'3.9'")
+requirements = open(os.path.join(this_dir, "requirements.txt"), "r", encoding="utf-8").read().strip().split()
 
 setup_args = {
     "name": "elapid",
