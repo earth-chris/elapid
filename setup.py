fd53326f1b610a9cc448c8f9bbe7e49074fe5739
earth-chris <cba@salo.ai>
2022-09-08 18:52:28 -0700

    moving all dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 import os
+import platform
 
 from setuptools import setup
 
 this_dir, this_path = os.path.split(os.path.abspath(__file__))
 version = open(os.path.join(this_dir, "elapid", "__version__.py")).read().strip('"\n')
 long_description = open(os.path.join(this_dir, "README.md"), "r", encoding="utf-8").read()
-requirements = open(os.path.join(this_dir, "requirements.txt"), "r", encoding="utf-8").read().strip().split()
+
+requirements = [
+    "descartes>=1.1.0",
+    "geopandas>=0.7.0",
+    "numpy>=1.18",
+    "pandas>=1.0.3",
+    "rasterio>=1.2.1",
+    "rtree>=0.9",
+    "scikit-learn>=0.22.2",
+    "shapely>=1.7.0",
+    "tqdm>=4.60.0",
+]
+if platform.system() != "Windows":
+    requirements.append("glmnet;python_version<'3.9'")
 
 setup_args = {
     "name": "elapid",
@@ -28,7 +42,7 @@ setup_args = {
     "packages": ["elapid"],
     "include_package_data": True,
     "install_requires": requirements,
-    "python_requires": ">=3.7.0,<3.9.0",
+    "python_requires": ">=3.7.0",
     "platforms": "any",
     "classifiers": [
         "Programming Language :: Python :: 3",
